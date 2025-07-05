bca856052a73bbb8aab3ac7f6f2997f8d75730dd
Tharusha Kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2025-07-05 20:44:29 +0530

    Update index.php

--- Task 3/index.php
+++ Task 3/index.php
@@ -1,13 +1,1 @@
-<?php
-$host = '34.10.65.134';        // Your DB public IP
-$db   = 'wordpress';           // DB name
-$user = 'wp_user';             // DB user
-$pass = 'bDlBh3ItC2wHzEL';         // DB password
 
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    echo "❌ Connection failed: " . $conn->connect_error;
-} else {
-    echo "✅ Successfully connected to MySQL!";
-}
